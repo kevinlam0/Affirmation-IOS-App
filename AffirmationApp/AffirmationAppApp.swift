648473c0f0d021384cf38dbc7dbdb2d765dfe699
kevinlam0 <kevinclam02@gmail.com>
2024-01-08 12:32:14 -0500

    Initial Commit

---
+++ AffirmationApp/AffirmationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationAppApp.swift
+//  AffirmationApp
+//
+//  Created by Kevin Lam on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AffirmationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
